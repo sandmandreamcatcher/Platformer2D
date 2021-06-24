d9c787da65f82214627d48e7df51db1a3f94a387
sandmandreamcatcher <dr.catcher@ukr.net>
2021-06-24 12:01:18 +0300

    Death added

--- Platformer/Assets/Scripts/Platform.cs
+++ Platformer/Assets/Scripts/Platform.cs
@@ -4,9 +4,5 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    //private void OnTriggerExit2D(Collider2D _current)
-    //{
-    //    if (_verticalLift)
-    //        _currentLift.GetComponent<Rigidbody2D>().gravityScale *= -1;
-    //}
+
 }
--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(BoxCollider2D))]
 
@@ -8,19 +9,29 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Player _player;
     [SerializeField] private LayerMask _layerMask = new LayerMask();
+    [SerializeField] private LayerMask _deathLayerMask = new LayerMask();
     [SerializeField] private BoxCollider2D _cellingCheckCollider;
     [SerializeField] private BoxCollider2D _floorCheckCollider;
+    [SerializeField] private Animator _animator;
     private float _castDistance = -1;
+    public bool IsDead { get; private set; }
 
     public bool IsGrounded { get; private set; }
 
     private void Awake()
     {
         _player = gameObject.GetComponent<Player>();
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void CheckGround()
+    {
+        IsGrounded = Physics2D.BoxCast(_floorCheckCollider.bounds.center, _floorCheckCollider.bounds.size, 0f, Vector2.up, _castDistance, _layerMask);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        CheckDeath();
         IsGrounded = true;
     }
 
@@ -29,8 +40,19 @@ public class Player : MonoBehaviour
         IsGrounded = false;
     }
 
-    public void CheckGround()
+    private void CheckDeath()
     {
-        IsGrounded = Physics2D.BoxCast(_floorCheckCollider.bounds.center, _floorCheckCollider.bounds.size, 0f, Vector2.up, _castDistance, _layerMask);
+        IsDead =_cellingCheckCollider.IsTouchingLayers(_deathLayerMask) || _floorCheckCollider.IsTouchingLayers(_deathLayerMask);
+        if (IsDead)
+        {
+            Debug.Log("YOU ARE DEAD!");
+            _animator.SetBool("PlayerDead", true);
+            ReloadCurrentScene();
+        } 
+    }
+
+    private void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
